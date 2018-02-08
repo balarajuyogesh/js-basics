0a3464bb9982ce309be2859150b1b5071c128bd6
Raju <balarajuyogesh@gmail.com>
2018-02-08 21:36:28 +0530

    one file added

--- loops/range-function.js
+++ loops/range-function.js
@@ -1,11 +1,13 @@
 let num1 = parseInt(window.prompt("Enter the first number", "0")),
     num2 = parseInt(window.prompt("Enter the second number", "0"));
 
-var range = function (num1, num2) {
-  if (num2 - num1 === 2) {
+let range = function (num1, num2) {
+  if (num2 - num1 === 0 || num2 - num1 ===1) {
+    console.log(`There are no integers in between ${num1} and ${num2}`);
+  } else if (num2 - num1 === 2) {
     return [num1 + 1]; 
   } else {
-    var list = range(num1, num2 - 1);
+    let list = range(num1, num2 - 1);
     list.push(num2 - 1);
     return list;
   }
